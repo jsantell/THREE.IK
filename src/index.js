33ca929e2ec3e7b59848962956fdd523e8cf8ed9
Jordan Santell <jordan@jsantell.com>
2018-04-22 20:33:20 -0700

    Bind export on THREE namespace when included in script tag

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ import IKJoint from './IKJoint.js';
 import IKBallConstraint from './IKBallConstraint.js';
 import IKHelper from './IKHelper.js';
 
+// If this is being included via script tag and using THREE
+// globals, attach our exports to THREE.
+if (typeof window !== 'undefined' && typeof window.THREE === 'object') {
+  window.THREE.IK = IK;
+  window.THREE.IKChain = IKChain;
+  window.THREE.IKJoint = IKJoint;
+  window.THREE.IKBallConstraint = IKBallConstraint;
+  window.THREE.IKHelper = IKHelper;
+}
+
 export {
   IK, IKChain, IKJoint, IKBallConstraint, IKHelper
 };
